ac97e4ba8361d2389c26be1921a038c29c48961e
Ilja Kahlert <kahlert@Norwa.local>
2018-01-23 16:56:08 +0100

    new docstring for point instance methods

--- point.py
+++ point.py
@@ -29,21 +29,26 @@ class GeoPoint(object):
         elif (self.east >= 3000) and (self.east <= 49800):
             if (self.north >= 800) and (self.north <= 39200):
                 return "500"
-        else:
-            return "000"
 
-    def set_coosys(self, csys="000"):
-        """
-        sets the code of the coordinate system and updates automatically
-        the code of the numbering area
-        """
-        self.csys = str(csys).ljust(3)[:3]
-        self.set_numbez()
+        return "000"
 
     def reset_coo(self, east, north, elev=None, csys=None, infercs=False):
+        """resets coordinate values and optionally code of the coordinate system inplace
+
+        Parameters:
+        ----------
+        east, north : float
+            the point's 2D coordinate values
+        elev : float, optional
+            elevation or height of point (the default is None, which mean point is 2D)
+        csys : 3-digit-string, optional
+            code for the points reference system 'Lagestatus'
+            (the default is None, which means reference system is not known)
+        infercs : bool, optional
+            states whether `csys` shall be inferred from coordinate values
+            (the default is False, which implies no inference)
         """
-        resets coordinate values and optionally code of the coordinate system inplace
-        """
+
         self.east = east
         self.north = north
         if elev:
@@ -51,25 +56,56 @@ class GeoPoint(object):
         if csys:
             self.set_coosys(csys)
         elif infercs:
-            self.set_coosys(self.infercs())
+            self.set_coosys(self.infer_csys())
+
+    def set_coosys(self, csys="000"):
+        """sets the code of the coordinate system and updates automatically
+        the code of the numbering area
+
+        Parameters:
+        ----------
+        csys : 3-digit-string, optional
+            code for the points reference system 'Lagestatus'
+            (the default is "000", which stands for a local reference system)
+
+        """
+
+        self.csys = str(csys).ljust(3)[:3]
+        self.set_numbez()
 
     def set_code(self, code):
+        """sets pointcode and extended pointcode (used in BAVs)
+
+        Parameters:
+        ----------
+        code : 3 digit int >= 100
+            point code that describes the point type
         """
-        sets pointcode and extended pointcode (used in BAVs)
-        """
+
         self.code = str(code).ljust(3)[:3]
         self.xcode = '5' + self.code
 
     def set_desc(self, desc):
+        """sets description string for point
+
+        Parameters:
+        ----------
+        desc : string
+            short description text with not more than 13 letters
         """
-        sets description string for point
-        """
+
+
         self.desc = str(desc).ljust(13)[:13]
 
     def set_numbez(self, numbez=None):
+        """sets code of numbering area
+
+        numbez : unsigned int, optional
+            numbering area code
+            (the default is None, for code inference or setting of a default value)
+
         """
-        sets code of numbering area
-        """
+
         if numbez:
             self.numbez = str(numbez).rjust(8, '0')[:8]
         else:
@@ -79,9 +115,14 @@ class GeoPoint(object):
                 self.numbez = '00000000'
 
     def to_bav(self):
+        """generates point representation used in BAV coordinate files
+
+        Returns
+        -------
+        string
+            point data in 1 line string
         """
-        generates point representation used in BAV coordinate files
-        """
+
         retval = ("{}      {} {} {:^10}   Y {:>12.3f} "
                   "X {:>13.3f} Z {:>9.3f} C {} {:<13}")
 
