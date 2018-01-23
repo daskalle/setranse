003d97cd57fac0be4161e84647450abe42f1e530
daskalle <daskalle@wolke7.net>
2018-01-23 21:44:09 +0100

    factor out regex pattern generation

--- cooreader.py
+++ cooreader.py
@@ -9,16 +9,11 @@ import point
 class CooReader:
     """Reads Coordinates from BAV and TXT files"""
 
-    def __init__(self, kind='bav'):
+    def __init__(self, fmt='bav', sep=' '):
         super(CooReader, self).__init__()
-        self.kind = kind
-        bavstring = r"^(?P<sys>\d{1,3}) +(?P<xcode>\d.{3}) +(?P<nb>\d+) +"
-        bavstring += r"(?P<name>[\w\.]+) +y +(?P<ost>\d+\.\d+) +x +"
-        bavstring += r"(?P<nord>\d+\.\d+) +z +(?P<z>\d+\.\d+)? +c +"
-        bavstring += r"(?P<pktcode>.{3}) *(?P<beschr>[\w\.-]* *[\w\.-]*){0,1}"
-
-        self.rex_bav = re.compile(bavstring, re.IGNORECASE)
-
+        self.fmt = fmt
+        self.sep = sep
+        self.rex = self.build_read_pattern()
 
     def readdata(self, filename):
         '''
@@ -41,7 +36,7 @@ class CooReader:
         '''
         todo docstring
         '''
-        match = self.rex_bav.match(line)
+        match = self.rex.match(line)
 
         ptnr = match.group('name')
         east = float(match.group('ost'))
@@ -66,7 +61,28 @@ class CooReader:
 
         return pnt
 
+    def build_read_pattern(self):
+        if self.fmt.lower() == 'bav':
+            bavstring = r"^(?P<sys>\d{1,3}) +(?P<xcode>\d.{3}) +(?P<nb>\d+) +"
+            bavstring += r"(?P<name>[\w\.]+) +y +(?P<ost>\d+\.\d+) +x +"
+            bavstring += r"(?P<nord>\d+\.\d+) +z +(?P<z>\d+\.\d+)? +c +"
+            bavstring += r"(?P<pktcode>.{3}) *(?P<beschr>[\w\.-]* *[\w\.-]*){0,1}"
 
-cr = CooReader()
+            return re.compile(bavstring, re.IGNORECASE)
 
-cr.readdata('17-005-lp-kv.bav')
+        else:
+            pattern = {'p': r'(?P<name>\w[\w\.]*)',
+                       'r': r'(?P<ost>-?\d+\.\d+)',
+                       'h': r'(?P<nord>-?\d+\.\d+)',
+                       'z': r'(?P<z>-?\d+\.\d+)',
+                       'c': r'(?P<pktcode>(\d|_){3})',
+                       'b': r'(?P<beschr>[\w\.-]{,13})',
+                       't': r'(?P<text>[\w\.- ]+)'}
+
+            fmtkeys = list(self.fmt.lower())
+
+            fmtstring = [pattern[key] for key in fmtkeys]
+
+            fmtstring = self.sep.join(fmtstring)
+
+            return re.compile(fmtstring, re.IGNORECASE)
